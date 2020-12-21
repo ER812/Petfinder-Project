883ea684c1ee8b19a32431516cfa31b4820ad4db
Elizabeth <Elizabeth.Rights812@gmail.com>
2020-12-21 15:12:19 -0500

    set-up

--- app.js
+++ app.js
@@ -2,72 +2,69 @@
 // {"token_type":"Bearer","expires_in":3600,"access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI4RVFWQzJSWDd6SDZSUWJBQlcxREN3bDkwUklKTTlwWUtoOW4zUzZydW85aUZPZFZJNSIsImp0aSI6IjM3OGU0ZWY1ZDJkZDFkMDkzMjA1NTc4MGY5Njc0YWEzN2IyMmQyZWFiYjY1OTljM2NiMjA2ZjJkODEzNDFhOGRmYmM1MWQzOTdiYWZiYWJkIiwiaWF0IjoxNjA4NTYyNTAzLCJuYmYiOjE2MDg1NjI1MDMsImV4cCI6MTYwODU2NjEwMywic3ViIjoiIiwic2NvcGVzIjpbXX0.Bn3dqNxewbXQEevcyVBkQlK1m7FAF8cggQESCrD67yl9AJMkpALTzZ6aU8KHoU93adUUtnrqcU81RB5r85kQp9s1tUfzYASBhMy7pOONFe_240pA84cHAUbYzDtDRHNa3wcPWUi70iCMuv8VwDaB9wz_nU7gThdxnUr32JjxVk4Ury3Tb4oR2UYa-jNHE9NxArRiH2TRnyk-vv75OGQoq4aJ-W1nQq2GDl4RJHm1RIJjYuUK5YdaXETF_6WGk-M5sfhBuc5joWiGfAYUb5iJr32NZHRiYDhBDKz_jm7NTr5n6l9Ekod_tf1ldIJuWSJKGZEIvJqjg5C5P0pAfefq0A"}
 
 // Conecting API to webpage:
-//Commenty comment
 
-async function getData() {
-  const url = "https://api.petfinder.com/v2/animals/"
-  console.log("is this working? api address")
-  
-  const petsList = await axios.get(url)
-  const res = Object.keys(petsList.data.message)
-  console.log("is this working? res const")
-  console.log(res)
-  
-  res.forEach(async (pet) => {
-    let newH1 = document.createElement("h1")
-    newH1.innerText = `${type}`
-    document.body.append(newH1)
-    console.log("is this working? innertext/forEach")
-   
-    const imgURL = `https://api.petfinder.com/v2/animals/`
-    const response = await axios.get(imgURL)
-    const imageSource = response.data.message
-    const image = document.createElement("img")
-    console.log(image)
-    image.setAttribute("src", imageSource)
-    document.getElementById("images").append(image)
+const getToken = async () => {
+  const response = await axios.post("https://api.petfinder.com/v2/oauth2/token",
+    {
+      client_id: '8EQVC2RX7zH6RQbABW1DCwl90RIJM9pYKh9n3S6ruo9iFOdVI5',
+      client_secret: 'JDoN4QpPMbqaXjQOfQTPZl1L9CyrZHW3LXeMaEkm',
+      grant_type: "client_credentials"
+    })
+  return response.data.access_token 
+}
+
+const getAnimals = async () => {
+  const token = await getToken()
+  const response = await axios.get("https://api.petfinder.com/v2/animals",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+    }
   })
-
-  getData()
-
-// Random adoptable pet on landing: 
-
-function animalPic() {
-  let image = document.createElement('img')
-  img.src = 
-  document.append(img)
-
+console.log(response.data.animals)
 }
 
-function optionValue(list) {
-  const select = document.querySelector('')
-  return list.forEach() => {
-    const option = document.createElement('option')
-    option.value = `${}`
-    option.textContent = `${}`
-    select.append(option)
+getAnimals()
 
-  }
+
+// // Random adoptable pet on landing: 
+
+// // function animalPic() {
+// //   let image = document.createElement('img')
+// //   // img.src = 
+// //   // document.append(image)
+
+// // }
+
+// // function optionValue() {
+// //   const select = document.querySelector('')
+// //   return list.forEach() => {
+// //     const option = document.createElement('option')
+// //     option.value = `${}`
+// //     option.textContent = `${}`
+// //     select.append(option)
+
+// //   }
   
-} 
+// // } 
 
-// Choose dog or cat: 
+// // Choose dog or cat: 
 
 
-// Search for Breed:
+// // Search for Breed:
 
-  // Get value from form
+//   // Get value from form
 
-function getValue(e) => {
-  e.preventDefault()
-  const optionValue = document.querySelector('').value
-  getBreed(optionValue)
-}
+// function getValue(e) => {
+//   e.preventDefault()
+//   const optionValue = document.querySelector('').value
+//   getBreed(optionValue)
+// }
 
-  // Event handler for the form
+//   // Event handler for the form
 
-const form = document.querySelector('form')
-form.addEventListener('submit', getValue)
+// const form = document.querySelector('form')
+// form.addEventListener('submit', getValue)
 
 
 
